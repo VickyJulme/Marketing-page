d9b6cc4a65ec57c3041608e2f32a135dad49135d
vikrant julme <VICKYJULME01@GMAIL.COM>
2024-08-12 16:56:01 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import DataScience from './assets/DataScience/DataHome'
+import Marketing from './assets/Marketing/Marketing'
+const App = () => {
+  return (
+    <div>
+{/* <DataScience/> */}
+<Marketing/>
+    </div>
+  )
+}
+
+export default App
